481ea3e728a05e31e01bb109d7087c90caa48ceb
Jon Errasti <jon44errasti@gmail.com>
2025-02-23 12:20:19 +0100

    Solved some import issues.

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    './frontend/src/**/*.{js,jsx,ts,tsx,html}',
+    './frontend/public/index.html',
+    './backend/src/**/*.{js,ts}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
